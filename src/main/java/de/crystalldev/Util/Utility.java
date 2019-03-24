be7fda4ec81aebd9bc2ad75bc305ca8e81949ef3
CrystallDEV <m.hasselder@gmx.de>
2019-03-24 02:43:16 +0100

    Added settings page

--- src/main/java/de/crystalldev/Util/Utility.java
+++ src/main/java/de/crystalldev/Util/Utility.java
@@ -1,23 +1,10 @@
-package de.crystalldev.util;
+package de.crystalldev.Util;
 
-import de.crystalldev.models.PlayerPlanet;
-
-import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Utility {
-    public final static String ESPIONAGEFILE = "spyreports";
-    public static String userName;
-    public static String eMail;
-    public static String password;
-    public static String server;
-    public static String serverAddress;
-    public static int actionsSinceAttackDetected = 0;
-    public static ArrayList<PlayerPlanet> playerPlanets = new ArrayList<>();
-    public static String activePlanet;
-
     /**
      * Lets the thread sleep for a random amount of time
      * @param milliseconds
